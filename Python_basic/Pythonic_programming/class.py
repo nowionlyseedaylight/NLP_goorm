4faefad2d58c1175b7eada481c0f2ae3b8e3ba10
Nahyun Kim <97136880+nowionlyseedaylight@users.noreply.github.com>
2022-08-16 22:03:17 +0900

    Create class.py

---
+++ Python_basic/Pythonic_programming/class.py
@@ -0,0 +1,8 @@
+class FourCal:
+  def setdata(self, first, second):  # 사칙연산을 위해 두 숫자를 입력 받아
+    self.first=first
+    self.second=second
+    
+   def add(self):
+    result=self.first + self.second   # 더하기 기능
+    return result
